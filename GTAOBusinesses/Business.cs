8e2e3e6ed8266d6c8b0896d2676477d0646a6564
Benjamin Goisser <benjamin.goisser.bg@gmail.com>
2022-04-14 22:30:22 +0200

    Fix resupply timer value not being loaded on launch

--- GTAOBusinesses/Business.cs
+++ GTAOBusinesses/Business.cs
@@ -176,6 +176,7 @@ namespace GTAOBusinesses
         {
             ToggleResupply();
             resupplyCounter = resupplyTimeLeft;
+            updateUI();
         }
 
         public void ToggleResupply()
